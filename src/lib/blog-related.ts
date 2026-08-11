ed9655a572f3b260c138203fd8e31bd6b6906060
Fanzheren <fanzheren@outlook.com>
2026-08-11 10:36:52 +0800

    fix: use series taxonomy for related posts

--- src/lib/blog-related.ts
+++ src/lib/blog-related.ts
@@ -1,11 +1,22 @@
 import type { CollectionEntry } from 'astro:content'
+import { blogSeries } from '../data/blog-series'
 
 type BlogEntry = CollectionEntry<'blog'>
+
+const seriesByCategory = new Map(
+  blogSeries.flatMap((series) => series.categoryIds.map((categoryId) => [categoryId, series.id] as const)),
+)
+
+const getSeriesIds = (post: BlogEntry) => new Set([
+  ...(post.data.series ? [post.data.series] : []),
+  ...(seriesByCategory.has(post.data.category) ? [seriesByCategory.get(post.data.category)!] : []),
+])
 
 const scoreRelatedPost = (post: BlogEntry, candidate: BlogEntry) => {
   let score = 0
   if (post.data.category === candidate.data.category) score += 5
-  if (post.data.series && post.data.series === candidate.data.series) score += 8
+  const postSeries = getSeriesIds(post)
+  if ([...getSeriesIds(candidate)].some((series) => postSeries.has(series))) score += 8
 
   const tags = new Set(post.data.tags)
   score += candidate.data.tags.reduce((total, tag) => total + (tags.has(tag) ? 2 : 0), 0)
